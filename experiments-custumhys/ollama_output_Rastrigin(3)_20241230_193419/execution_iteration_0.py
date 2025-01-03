dcce8d94aaa1d138fce4bb601b8fcb94db7bce65
Valenriquez <enriquezlimvaleria@outlook.com>
2025-01-02 23:06:51 -0600

    Will get last values of np.array(fitness).T for fitness comparison

--- experiments-custumhys/ollama_output_Rastrigin(3)_20241230_193419/execution_iteration_0.py
+++ experiments-custumhys/ollama_output_Rastrigin(3)_20241230_193419/execution_iteration_0.py
@@ -49,14 +49,15 @@ met.verbose = True
 fitness = []
 # Run the metaheuristic with the same problem 30 times
 for rep in range(30):
-    met = mh.Metaheuristic(prob, heur, num_iterations=1000, num_agents=10)
+    met = mh.Metaheuristic(prob, heur, num_iterations=100, num_agents=30)
     met.reset_historicals()
     met.verbose = False
     met.run()
     print('rep = {}, x_best = {}, f_best = {}'.format(rep+1, *met.get_solution()))
     
     fitness.append(met.historical['fitness'])
-    
+print(fitness)
+
 # Short explanation and justification:
 # This hybrid metaheuristic combines three different search operators: 
 # 1. Random Search for exploration,
